d4adece55ef25b24f60f3a43fe8e0fb2040d9975
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-02-05 12:04:04 -0300

    Add files via upload
    Semana 5 y 6

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 1',
-    data: [16, 20, 15.2, 5], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 5',
+    data: [18, 13, 11, 10.5], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: false,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 2',
-    data: [15, 7, 18, 9.4], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 6',
+    data: [14, 15, 9, 20], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: false,
     tension: 0.1
