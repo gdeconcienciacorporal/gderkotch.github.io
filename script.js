9abb2a28bfece4a32863f540d82fbf684712acb9
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-03-03 14:32:28 -0300

    Semana 9 y 10

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 7',
-    data: [17, 22, 13, 24], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 9',
+    data: [20, 15, 19, 22], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: false,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 8',
-    data: [20, 15, 19, 22], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 10',
+    data: [11, 17, 17, 13], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: false,
     tension: 0.1
