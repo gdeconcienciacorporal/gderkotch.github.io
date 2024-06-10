4846420b77b33f86d147ae22f24def9379903a42
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-06-09 23:25:52 -0300

    Commit weeks 17 & 18

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //actualizarDiasRestantes();
-  //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
-   // Datos para el gráfico de líneas
+ // actualizarDiasRestantes();
+ //setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario 
+ // Datos para el gráfico de líneas
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 15',
-    data: [15, 15, 17, 19], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 17',
+    data: [17, 19, 27, 17], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -14,10 +14,10 @@ const dataLine1 = {
 };
 
 const dataLine2 = {
-  labels: ['Día 1', 'Día 2', 'Día 3','Día 4'], // Etiquetas de los días], // Etiquetas de los días
+  labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 16',
-    data: [13, 17, 19, 17], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 18',
+    data: [14, 15, 20, 15], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -25,7 +25,8 @@ const dataLine2 = {
 };
 
 // Configuración del gráfico de líneas
-const configLine = {
+// Seleccione los contextos de los <canvas> y cree los gráficos
+const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), {
   type: 'line',
   data: dataLine1, // Puede cambiar esto a dataLine2 para mostrar el gráfico de la segunda sección
   options: {
@@ -35,11 +36,9 @@ const configLine = {
       }
     }
   }
-};
+});
 
-// Seleccione los contextos de los <canvas> y cree los gráficos
-const myChartLine1 = new Chart(document.getElementById('myChart-line-1'), configLine);
-const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
+ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
   type: 'line',
   data: dataLine2,
   options: {
@@ -51,22 +50,16 @@ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
   }
 });
 
-function actualizarDiasRestantes() {
-  // Obtener el título que contiene la fecha objetivo
+function actualizarDiasRestantes() { // Obtener el título que contiene la fecha objetivo
   const pageTitle =  document.getElementById('pageTitle').innerText;
-    
   // Extraer la fecha de la cadena del título
   const objetivoDate = new Date(pageTitle.split(": ")[1]);
-
   // Obtener la fecha actual
   const currentDate = new Date();
-
   // Calcular la diferencia en milisegundos entre las dos fechas
   const timeDiff = objetivoDate - currentDate;
-
   // Calcular la cantidad de días
   const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
-
   // Mostrar el resultado
   const resultElement = document.getElementById('result');
   resultElement.textContent = `Faltan ${daysDiff} días para llegar a la carrera.`;
@@ -109,4 +102,3 @@ function actualizarDiasRestantes() {
   document.getElementById("Semana 2").appendChild(video4);
 
 });
-
