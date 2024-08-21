abecf9fe1818c0c967023e831d6606db88c6d7da
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-08-20 22:26:09 -0300

    Commit weeks 29 & 30

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 27',
-    data: [15, 16, 27, 18], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 29',
+    data: [21, 19, 30 , 12], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 28',
-    data: [21, 18, 29, 18], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 30',
+    data: [12, 23, 27, 12], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
